b90a436a19a808be917fc03768d586e9520e63f6
rsawejka <ryansawejka1999@gmail.com>
2024-04-01 19:21:36 -0500

    about me page

--- includes/aboutMe.php
+++ includes/aboutMe.php
@@ -1,8 +1,11 @@
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4" id="selfPic"></div>
 <?php
 include "aboutMeTable.php";
 //see notebook
+
+
  echo '<div class="container ">';
-echo ' <div class="row mb-5">';
+echo ' <div class="row justify-content-between mb-5">';
 
 // foreach ($information as $info){
 // echo '<div class="' . $info[3] . " " . ' aboutMeBox">
@@ -12,11 +15,20 @@ echo ' <div class="row mb-5">';
 //       </div>';
 // }
 // ?>
-<div id="selfPic"></div>
-<div id="selfIntroText"> I am a web developer with 5 years+ of experiance. For me coding is a fun way for me to push my self to continually keep learning. 
+
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4" id="selfIntroText"> I am a web developer with 5 years+ of experiance. For me coding is a fun way for me to push my self to continually keep learning. 
         I am always excited to take on new tasks as they are an oppertunity to learn more. I thrive the most when I am being challanged, when I am challanged I am very determined and excited to figure out the solution. 
         Even when I am being challanged I make sure I am still paying close attention to the small deatils to ensure quailty. Currently I am a web developer at the Humane Animal Welfare Society in Waukesha, WI. 
         I like being able to be creative in helping create new ways to get more animals adoped faster! I am also able to help create new ways to even improve animal health.
+      </div>
+<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4" id="myActivities"> 
+    <h3>Free Time Activites</h3>
+    <ul>
+      <li>SnowBoarding</li>
+      <li>Working with computers</li>
+      <li>Coding</li>
+      <li>Video Games</li>
+    </ul>
       </div>
 </div>
 </div> 
